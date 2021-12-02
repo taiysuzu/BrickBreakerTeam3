eee1040ca38bbd03878a7b541e10fae6489c7ecc
taiysuzu <74975096+taiysuzu@users.noreply.github.com>
2021-12-02 11:17:08 -0500

    g

--- BrickBreaker/Screens/HowtoPlayScreen.designer.cs
+++ BrickBreaker/Screens/HowtoPlayScreen.designer.cs
@@ -29,38 +29,12 @@ namespace BrickBreaker
         /// </summary>
         private void InitializeComponent()
         {
-            this.howtolabel = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.backButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // howtolabel
-            // 
-            this.howtolabel.BackColor = System.Drawing.Color.White;
-            this.howtolabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.howtolabel.ForeColor = System.Drawing.Color.Black;
-            this.howtolabel.Location = new System.Drawing.Point(61, 50);
-            this.howtolabel.Name = "howtolabel";
-            this.howtolabel.Size = new System.Drawing.Size(287, 153);
-            this.howtolabel.TabIndex = 0;
-            this.howtolabel.Text = "Arrow Keys (←→) To Move!\r\n\r\nDont Let the Ball Touch the Ground!\r\n\r\nYou Have 3 Liv" +
-    "es!\r\n";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.BackgroundImage = global::BrickBreaker.Properties.Resources.realbubble;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(28, 30);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(333, 213);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
             // 
             // backButton
             // 
-            this.backButton.Location = new System.Drawing.Point(333, 344);
+            this.backButton.Location = new System.Drawing.Point(298, 336);
             this.backButton.Name = "backButton";
             this.backButton.Size = new System.Drawing.Size(181, 102);
             this.backButton.TabIndex = 3;
@@ -75,19 +49,13 @@ namespace BrickBreaker
             this.BackgroundImage = global::BrickBreaker.Properties.Resources.howtoplay;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Controls.Add(this.backButton);
-            this.Controls.Add(this.howtolabel);
-            this.Controls.Add(this.pictureBox2);
             this.Name = "HowtoPlayScreen";
             this.Size = new System.Drawing.Size(854, 542);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label howtolabel;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button backButton;
     }
 }
