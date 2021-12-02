5c5a1a3ba1bc1998c19ebf63d988c21b40d446d9
taiysuzu199@outlook.com <taiysuzu199@outlook.com>
2021-12-02 12:13:30 -0500

    changes complete

--- BrickBreaker/Properties/Resources.Designer.cs
+++ BrickBreaker/Properties/Resources.Designer.cs
@@ -212,45 +212,6 @@ namespace BrickBreaker.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot; ?&gt;
-        ///&lt;level&gt;
-        ///  &lt;brick&gt;
-        ///    &lt;x&gt;25&lt;/x&gt;
-        ///    &lt;y&gt;162&lt;/y&gt;
-        ///    &lt;hp&gt;1&lt;/hp&gt;
-        ///    &lt;colour&gt;4&lt;/colour&gt;
-        ///    &lt;type&gt;0&lt;/type&gt;
-        ///  &lt;/brick&gt;
-        ///  &lt;brick&gt;
-        ///    &lt;x&gt;56&lt;/x&gt;
-        ///    &lt;y&gt;153&lt;/y&gt;
-        ///    &lt;hp&gt;1&lt;/hp&gt;
-        ///    &lt;colour&gt;4&lt;/colour&gt;
-        ///    &lt;type&gt;0&lt;/type&gt;
-        ///  &lt;/brick&gt;
-        ///  &lt;brick&gt;
-        ///    &lt;x&gt;87&lt;/x&gt;
-        ///    &lt;y&gt;144&lt;/y&gt;
-        ///    &lt;hp&gt;1&lt;/hp&gt;
-        ///    &lt;colour&gt;4&lt;/colour&gt;
-        ///    &lt;type&gt;0&lt;/type&gt;
-        ///  &lt;/brick&gt;
-        ///  &lt;brick&gt;
-        ///    &lt;x&gt;118&lt;/x&gt;
-        ///    &lt;y&gt;135&lt;/y&gt;
-        ///    &lt;hp&gt;1&lt;/hp&gt;
-        ///    &lt;colour&gt;4&lt;/colour&gt;
-        ///    &lt;type&gt;0&lt;/type&gt;
-        ///  &lt;/brick&gt;
-        ///  &lt;br [rest of string was truncated]&quot;;.
-        /// </summary>
-        internal static string level1 {
-            get {
-                return ResourceManager.GetString("level1", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot; ?&gt; 
         ///&lt;level&gt;
         ///  &lt;brick&gt;
@@ -283,9 +244,9 @@ namespace BrickBreaker.Properties {
         ///  &lt;/brick&gt;
         ///  &lt;br [rest of string was truncated]&quot;;.
         /// </summary>
-        internal static string level2 {
+        internal static string level1 {
             get {
-                return ResourceManager.GetString("level2", resourceCulture);
+                return ResourceManager.GetString("level1", resourceCulture);
             }
         }
         
@@ -321,6 +282,45 @@ namespace BrickBreaker.Properties {
         ///    &lt;type&gt;1&lt;/type&gt;
         ///  &lt;/brick&gt;
         ///  &lt;brick&gt; [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string level2 {
+            get {
+                return ResourceManager.GetString("level2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot; ?&gt;
+        ///&lt;level&gt;
+        ///  &lt;brick&gt;
+        ///    &lt;x&gt;25&lt;/x&gt;
+        ///    &lt;y&gt;162&lt;/y&gt;
+        ///    &lt;hp&gt;1&lt;/hp&gt;
+        ///    &lt;colour&gt;4&lt;/colour&gt;
+        ///    &lt;type&gt;0&lt;/type&gt;
+        ///  &lt;/brick&gt;
+        ///  &lt;brick&gt;
+        ///    &lt;x&gt;56&lt;/x&gt;
+        ///    &lt;y&gt;153&lt;/y&gt;
+        ///    &lt;hp&gt;1&lt;/hp&gt;
+        ///    &lt;colour&gt;4&lt;/colour&gt;
+        ///    &lt;type&gt;0&lt;/type&gt;
+        ///  &lt;/brick&gt;
+        ///  &lt;brick&gt;
+        ///    &lt;x&gt;87&lt;/x&gt;
+        ///    &lt;y&gt;144&lt;/y&gt;
+        ///    &lt;hp&gt;1&lt;/hp&gt;
+        ///    &lt;colour&gt;4&lt;/colour&gt;
+        ///    &lt;type&gt;0&lt;/type&gt;
+        ///  &lt;/brick&gt;
+        ///  &lt;brick&gt;
+        ///    &lt;x&gt;118&lt;/x&gt;
+        ///    &lt;y&gt;135&lt;/y&gt;
+        ///    &lt;hp&gt;1&lt;/hp&gt;
+        ///    &lt;colour&gt;4&lt;/colour&gt;
+        ///    &lt;type&gt;0&lt;/type&gt;
+        ///  &lt;/brick&gt;
+        ///  &lt;br [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string level3 {
             get {
